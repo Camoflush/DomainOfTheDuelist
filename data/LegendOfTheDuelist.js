aa324f54fc62d3f2da712b8b3bfe46e95f2e6269
Camoflush <fabian.warth@freenet.de>
2025-03-17 17:22:28 +0100

    Add files via upload

---
+++ data/LegendOfTheDuelist.js
@@ -0,0 +1,21 @@
+const Legend of the Duelist = {
+    "cards": [{
+        "name": "",
+        "frame": "Rule",
+        "types": "",
+        "set": "Legend of the Duelist",
+        "cardid": "",
+        "image": "./cards/SOE/01.jpg",
+        "deck": "B",
+        "keywords": ""
+    }, {
+        "name": "Jonouchi",
+        "frame": "Rule",
+        "types": "",
+        "set": "Legend of the Duelist",
+        "cardid": "SOE-02",
+        "image": "./cards/SOE/02.jpg",
+        "deck": "B",
+        "keywords": ""
+    },]
+};
